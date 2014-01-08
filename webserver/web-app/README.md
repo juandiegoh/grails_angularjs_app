2857d5e02cf2daa548a66c275fdef7524e73a180
Juan Diego Herenu <jdherenu@gmail.com>
2014-01-07 22:49:32 -0300

    Adding Routes
    It just works as a stand alone web app.
    Working to make it work with Grails.

---
+++ webserver/web-app/README.md
@@ -0,0 +1,5 @@
+Turn on server:
+
+python -mSimpleHTTPServer
+
+connect to: localhost:8000
--- webserver/web-app/js/application.js
+++ webserver/web-app/js/application.js
@@ -1,4 +1,12 @@
-var configParamsApp = angular.module('configParamsApp', []);
+var configParamsApp = angular.module('configParamsApp', ['ngRoute']);
+
+configParamsApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+        when('/showConfigs', {templateUrl: 'templates/list.html',   controller: 'ConfigParamsController'}).
+        when('/newConfig', {templateUrl: 'templates/new.html', controller: 'ConfigParamsController'}).
+        when('/showConfig/:configId', {templateUrl: 'templates/show.html', controller: 'ConfigParamsController'}).
+        otherwise({redirectTo: '/showConfigs'});
+}]);
 
 configParamsApp.controller('ConfigParamsController', ['$scope', '$http',
     function ($scope, $http) {
@@ -7,55 +15,11 @@ configParamsApp.controller('ConfigParamsController', ['$scope', '$http',
 
     // load all configs, copying to the "configs" list on success
     $scope.loadConfigs = function() {
-        $http.get("configParams/list").success( function( data ) {
+        $http.get("http://localhost:8080/webserver/configParams/list").success( function( data ) {
             $scope.configs = data
         })
+        // $scope.configs = [{"class":"com.meli.configuration.ConfigParams","id":1,"active":true,"instanceAvailables":"e19","mainScopeFunctionality":"FACTORY","quantity":1001,"selectionId":0,"selectionType":"MOD"},{"class":"com.meli.configuration.ConfigParams","id":2,"active":true,"instanceAvailables":"e1","mainScopeFunctionality":"CLOSER","quantity":1000,"selectionId":0,"selectionType":"MOD"},{"class":"com.meli.configuration.ConfigParams","id":3,"active":true,"instanceAvailables":"e1","mainScopeFunctionality":"CLOSER","quantity":1000,"selectionId":1,"selectionType":"MOD"}]
     }
 
     $scope.loadConfigs()
     }]);
-//var configParamsApp = angular.module('configParamsApp', []);
-//
-////Define Routing for app
-////Uri /AddNewOrder -> template add_order.html and Controller AddOrderController
-////Uri /ShowOrders -> template show_orders.html and Controller AddOrderController
-//configParamsApp.config(['$routeProvider',
-//    function($routeProvider) {
-//        $routeProvider.
-//            when('/showConfigs', {
-//                templateUrl: 'templates/show_configs.html',
-//                controller: 'ShowConfigController'
-//            }).
-//            otherwise({
-//                redirectTo: '/showConfigs'
-//            });
-//    }]);
-//
-//
-//configParamsApp.controller('ShowConfigController', function($scope) {
-//
-//    $scope.message = 'This is Add new order screen';
-//
-//});
-//
-//
-//var configParamsApp = angular.module('configParamsApp', []);
-//
-//configParamsApp.controller('ConfigParamsController', ['$scope', '$http', function ($scope, $http) {
-//
-//    // bindable list of configs
-//    $scope.configs = []
-////        {active:true, instanceAvailables:'e1', mainScopeFunctionality:'FACTORY', quantity:1000, selectionId:0, selectionType:'MOD'},
-////        {active:true, instanceAvailables:'e1', mainScopeFunctionality:'CLOSER', quantity:1000, selectionId:0, selectionType:'MOD'},
-////        {active:true, instanceAvailables:'e1', mainScopeFunctionality:'CLOSER', quantity:1000, selectionId:1, selectionType:'MOD'}
-////    ]
-//
-//    // load all configs, copying to the "configs" list on success
-//    $scope.loadConfigs = function() {
-//        $http.get("configParams/list").success( function( data ) {
-//            $scope.configs = data
-//        })
-//    }
-//
-//    $scope.loadConfigs()
-//}]);
